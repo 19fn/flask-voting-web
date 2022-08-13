25ecc487781299c33a95200bfb642d06e6606a1f
19fn <fcabrera@gmail.com>
2022-08-13 01:44:03 -0300

    Create main logic

---
+++ web/routes.py
@@ -0,0 +1,21 @@
+from web import app
+from flask import render_template, request
+
+# Routes
+@app.route("/python-web-counter/home.html", methods=["GET", "POST"])
+def home_page():
+    msg = ""   
+    
+    if request.method == 'POST':
+        if request.form['sub_button'] == 'button_1':
+            msg = "GREEN"
+            return render_template("/home.html", msg = msg)
+        elif request.form['sub_button'] == 'button_2':
+            msg = "RED"
+            return render_template("/home.html", msg = msg)
+    return render_template("/home.html")
+
+# Errors
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
