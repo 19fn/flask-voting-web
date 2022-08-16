cbb0f8cb25dab564925dfee3913660106dbfe283
19fn <fcabrera@gmail.com>
2022-08-16 14:47:20 -0300

    Fix routes

--- web/routes.py
+++ web/routes.py
@@ -19,11 +19,6 @@ def home_page():
     btn2 = button_2()
 
     msg = ""
-    
-    # if request.method == 'GET':
-    #     butt1 = button_1.query.order_by(id.desc()).first()
-    #     butt2 = button_2.query.order_by(id.desc()).first()
-    #     return render_template("/home.html", butt1 = butt1, butt2 = butt2)
 
     if request.method == 'POST':
         if request.form['sub_button'] == 'button_1':
@@ -34,7 +29,7 @@ def home_page():
             )
             db.session.add(btn1)
             db.session.commit()
-            return render_template("/home.html", msg = msg, btn1 = btn1.clicks)
+            return render_template("/home.html", msg = msg, btn1 = btn1.clicks, btn2 = btn2.clicks)
         elif request.form['sub_button'] == 'button_2':
             msg = "RED"
             counter_btn_2 += 1
@@ -43,7 +38,7 @@ def home_page():
             )
             db.session.add(btn2)
             db.session.commit()
-            return render_template("/home.html", msg = msg, btn2 = btn2.clicks)
+            return render_template("/home.html", msg = msg, btn1 = btn1.clicks, btn2 = btn2.clicks)
     return render_template("/home.html", btn1 = btn1.clicks, btn2 = btn2.clicks)
 
 # Errors
