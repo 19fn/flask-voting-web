1dda110c361cd5a5062cf829ec95950e3c0afed2
19fn <fcabrera@gmail.com>
2022-08-25 18:44:48 -0300

    Add init value to the button table and change rows names

--- web/models.py
+++ web/models.py
@@ -1,10 +1,16 @@
 from web import db
 
-# Tables
+# button table
 class button(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    green_click = db.Column(db.Integer(), nullable=True)
-    red_click = db.Column(db.Integer(), nullable=True)
+    btn_1 = db.Column(db.Integer(), nullable=False)
+    btn_2 = db.Column(db.Integer(), nullable=False)
 
+# Create table
 db.create_all()
+db.session.commit()
+
+# Add init value to button table
+null_vote = button(btn_1=0, btn_2=0)
+db.session.add(null_vote)
 db.session.commit()
