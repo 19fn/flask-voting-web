c16e340a68593b30e08e4bc7059e08762292c61f
19fn <fcabrera@gmail.com>
2022-08-25 18:43:25 -0300

    Rename the connection string and add some comments

--- web/__init__.py
+++ web/__init__.py
@@ -3,12 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="templates")
 
-uid = "root"
-passwd = ""
-ip = ""
-database = "devtest_db"
+# SENSIBLE
+server_name = ""
+database_name = ""
+server_admin_login_name = ""
+server_admin_login_password = ""
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{uid}:{passwd}@{ip}:3306/{database}"
+# Database string connection
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{server_admin_login_name}:{server_admin_login_password}@{server_name}:3306/{database_name}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = "byiqpj0nZpJ+GnkPEFx86A=="
 
